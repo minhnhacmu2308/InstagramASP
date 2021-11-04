a848606b9987843c290202a4ef7c6946ff5aa8b6
nguyendolar <nguyencaonguyencmu@gmail.com>
2021-11-04 14:04:41 +0700

    up

---
+++ InstagramAspMVC/Controllers/AdminClientController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InstagramAspMVC.Dao;
+
+namespace InstagramAspMVC.Controllers.Admin
+{
+    public class AdminClientController : Controller
+    {
+        AdminUserDao adminU = new AdminUserDao();
+        // GET: AdminClient
+        public ActionResult Index(string mess)
+        {
+            ViewBag.Msg = mess;
+            var list = adminU.getAll();
+            return View(list);
+        }
+
+        public ActionResult ChangeStatus(int id)
+        {
+            adminU.changeStatus(id);
+            return RedirectToAction("Index", new { mess = "Thành công" });
+        }
+    }
+}
